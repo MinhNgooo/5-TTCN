3041de7db1e8ae4ab9b0a7514ce2dfcbb0240ca1
MustangNguyen <124245922+MustangNguyen@users.noreply.github.com>
2023-04-01 17:31:03 +0700

    them bai tap cau truc java don gian

---
+++ Bai tap java/Basic structure/java/src/bai1_47_nguyen_huu_quy/BinarySearch.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bai1_47_nguyen_huu_quy;
+
+/**
+ *
+ * @author iq123
+ */
+class BinarySearch {
+
+    BinarySearch(int[] array, int x, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
